6b68556db3b8fc7ed12f91045fb3bfaf6bc79fa5
elbuo8 <yamil.asusta@upr.edu>
2013-10-16 17:44:34 -0400

    Working version of outbout with kills and reporting

--- gamelogic.js
+++ gamelogic.js
@@ -1,10 +1,23 @@
+var sendgrid = require('sendgrid')(process.env.SG_USER, process.env.SG_PWD);
 
 module.exports = function (app) {
 
   app.get('io').sockets.on('connection', function (socket) {
     socket.on('join game', function (id) {
       socket.join(id);
-      app.get('io').sockets.in(id).emit('attack', 'ya@sendgrid.com');
+      app.get('io').sockets.in(id).emit('attack', 'yamil.asusta@upr.edu');
+    });
+    socket.on('damage', function (mail) {
+      sendgrid.send({
+        to: mail.email,
+        from: 'yamil.asusta@upr.edu',
+        subject: mail.damage,
+        text: mail.killed
+      }, function (e) {
+        if (e) {
+          console.log(e);
+        }
+      });
     });
   });
 };
