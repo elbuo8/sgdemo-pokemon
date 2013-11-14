421c6fdc294213a556fa00dd50a06fc55fc5522c
elbuo8 <yamil.asusta@sendgrid.com>
2013-11-14 15:29:32 -0500

    Routing

--- routes.js
+++ routes.js
@@ -2,5 +2,5 @@
 module.exports = function (app) {
   var gameController = require('./controllers/game')(app);
   app.get('/game/:id', gameController.gameview);
-  app.post('/game', gameController.attack);
+  app.post('/game/', gameController.attack);
 };
