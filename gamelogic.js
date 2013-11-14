d95c340b6533c29664286e7eacdf7cc4faa55a14
elbuo8 <yamil.asusta@sendgrid.com>
2013-11-14 12:34:43 -0500

    Simple adjustments for demo time

--- gamelogic.js
+++ gamelogic.js
@@ -5,14 +5,16 @@ module.exports = function (app) {
   app.get('io').sockets.on('connection', function (socket) {
     socket.on('join game', function (id) {
       socket.join(id);
-      app.get('io').sockets.in(id).emit('attack', 'yamil.asusta@upr.edu');
+      //app.get('io').sockets.in(id).emit('attack', 'yamil.asusta@upr.edu');
     });
     socket.on('damage', function (mail) {
       sendgrid.send({
         to: mail.email,
-        from: 'yamil.asusta@upr.edu',
-        subject: mail.damage,
-        text: mail.killed
+        from: 'yamil.asusta@sendgrid.com',
+        subject: 'hack.sendgrid.com',
+        text: 'You performed: ' + mail.damage + ' damage.\n' + mail.killed +
+        '\nHey buddy, thanks for participating on the SendGrid demo!\n' +
+        'Feel free to join using http://hack.sendgrid.com/ ! Its free!'
       }, function (e) {
         if (e) {
           console.log(e);
