554dcc5cebda9bce569a0d3f49d4299119676962
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-02-25 23:09:33 -0600

    Add redirect to '/game/20' when accessing '/'

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@
 module.exports = function (app) {
   var gameController = require('./controllers/game')(app);
   var retentionController = require('./controllers/retention')(app);
+  app.get('/', function(req,res) {
+    res.redirect('/game/20');
+  });
   app.get('/game/:lifepoints', gameController.gameview);
   app.post('/game/', gameController.attack);
   app.get('/emails', retentionController.compose);
